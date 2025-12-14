1545751b0f522b444047cad68144a2fd0fb3e9ed
quoc_bao <nqbbao1996@gmail.com>
2025-12-14 22:36:05 +0700

    fix: change question

--- constants.ts
+++ constants.ts
@@ -1,155 +1,118 @@
-
-import { Question } from './types';
+import { Question } from "./types";
 
 export const QUIZ_QUESTIONS: Question[] = [
   {
-    id: 1,
-    question: "Châu Âu có diện tích là:",
-    options: ["trên 7 triệu km²", "trên 8 triệu km²", "trên 9 triệu km²", "trên 10 triệu km²"],
-    correctAnswer: "trên 10 triệu km²"
-  },
-  {
-    id: 2,
-    question: "Con sông nào dài nhất ở châu Âu?",
-    options: ["Sông Rai-nơ", "Sông Đa-nuýp", "Sông Đni-ep", "Sông Von-ga"],
-    correctAnswer: "Sông Von-ga"
-  },
-  {
-    id: 3,
-    question: "Đặc điểm cơ cấu dân cư châu Âu là:",
-    options: ["cơ cấu dân số trẻ", "cơ cấu dân số già", "tỷ lệ nam nhiều hơn nữ", "Trình độ học vấn thấp"],
-    correctAnswer: "cơ cấu dân số già"
-  },
-  {
-    id: 4,
-    question: "Năm 2020, các đô thị nào ở châu Âu dưới đây có số dân từ 10 triệu người trở lên?",
-    options: ["Mat-xcơ-va, Pa-ri", "Xanh Pê-tec-bua, Ma-đrit", "Bec-lin, Viên", "Rô-ma, A-ten"],
-    correctAnswer: "Mat-xcơ-va, Pa-ri"
-  },
-  {
-    id: 5,
-    question: "Phần lớn lãnh thổ châu Âu thuộc đới khí hậu nào?",
-    options: ["Ôn hoà bán cầu Bắc", "Ôn hoà bán cầu Nam", "Nhiệt đới bán cầu Bắc", "Nhiệt đới bán cầu Nam"],
-    correctAnswer: "Ôn hoà bán cầu Bắc"
-  },
-  {
-    id: 6,
-    question: "Đô thị hóa ở châu Âu có đặc điểm nào sau đây?",
-    options: ["Mức độ đô thị hóa rất thấp", "Mức độ đô thị hóa thấp", "Chủ yếu là đô thị hóa tự phát", "Mức độ đô thị hóa cao"],
-    correctAnswer: "Mức độ đô thị hóa cao"
-  },
-  {
     id: 7,
-    question: "Dãy núi cao và đồ sộ nhất châu Âu là:",
-    options: ["Py-rê-nê", "Xcan-đi-na-vi", "An-pơ", "Cát-pát"],
-    correctAnswer: "An-pơ"
+    question:
+      "Từ bắc vào nam, phần đất liền nước ta kéo dài khoảng bao nhiêu vĩ độ?",
+    options: [
+      "15 độ vĩ tuyến",
+      "16 độ vĩ tuyến",
+      "17 độ vĩ tuyến",
+      "18 độ vĩ tuyến",
+    ],
+    correctAnswer: "15 độ vĩ tuyến",
   },
   {
     id: 8,
-    question: "Khu vực địa hình chiếm diện tích lớn nhất ở châu Âu là:",
-    options: ["đồng bằng", "núi già", "núi trẻ", "cao nguyên"],
-    correctAnswer: "đồng bằng"
+    question: "Địa hình nước ta có hướng nghiêng chung nào sau đây?",
+    options: [
+      "Tây - Đông",
+      "Bắc - Nam",
+      "Tây Bắc - Đông Nam",
+      "Đông Bắc - Tây Nam",
+    ],
+    correctAnswer: "Tây Bắc - Đông Nam",
   },
   {
     id: 9,
-    question: "Châu Âu thuộc lục địa?",
-    options: ["Phi", "Á - Âu", "Nam Mĩ", "Bắc Mĩ"],
-    correctAnswer: "Á - Âu"
+    question: "Ở Việt Nam, đồng bằng chiếm",
+    options: [
+      "2/3 diện tích đất liền",
+      "1/2 diện tích đất liền",
+      "3/4 diện tích đất liền",
+      "1/4 diện tích đất liền",
+    ],
+    correctAnswer: "1/4 diện tích đất liền",
   },
   {
     id: 10,
-    question: "Châu Á có số dân đông thứ mấy thế giới?",
-    options: ["1", "2", "3", "4"],
-    correctAnswer: "1"
+    question: "Khoáng sản nào sau đây thuộc khoáng sản năng lượng?",
+    options: ["Khí tự nhiên", "Man-gan", "Bô-xít", "Cao lanh"],
+    correctAnswer: "Khí tự nhiên",
   },
   {
     id: 11,
-    question: "Đới khí hậu cực và cận cực của châu Á phân bố ở khu vực:",
-    options: ["Đông Á", "Bắc Á", "Đông Nam Á", "Nam Á"],
-    correctAnswer: "Bắc Á"
+    question:
+      "Tính chất ẩm của khí hậu nước ta thể hiện qua các yếu tố nào sau đây?",
+    options: [
+      "Nhiệt độ và số giờ nắng",
+      "Lượng mưa và độ ẩm",
+      "Độ ẩm và cán cân bức xạ",
+      "Ánh sáng và lượng mưa",
+    ],
+    correctAnswer: "Lượng mưa và độ ẩm",
   },
   {
     id: 12,
-    question: "Ranh giới tự nhiên giữa châu Âu với châu Á là dãy núi:",
-    options: ["U-ran", "An-pơ", "Cac-pat", "Xcan-đi-na-vi"],
-    correctAnswer: "U-ran"
+    question: "Ở nước ta, loại gió nào sau đây thổi quanh năm?",
+    options: ["Tín phong", "Đông Bắc", "Tây Nam", "Đông Nam"],
+    correctAnswer: "Tín phong",
   },
   {
     id: 13,
-    question: "Sơn nguyên Tây Tạng phân bố ở khu vực nào của châu Á?",
-    options: ["Rìa phía bắc", "Rìa phía đông", "Vùng trung tâm", "Ven biển Địa Trung Hải"],
-    correctAnswer: "Vùng trung tâm"
+    question: "Phần biển Việt Nam có diện tích khoảng:",
+    options: [
+      "1,3 triệu km²",
+      "1,2 triệu km²",
+      "1,0 triệu km²",
+      "1,5 triệu km²",
+    ],
+    correctAnswer: "1,0 triệu km²",
   },
   {
     id: 14,
-    question: "Khu vực núi, cao nguyên và sơn nguyên chiếm:",
-    options: ["1/2 diện tích châu Á", "1/4 diện tích châu Á", "3/4 diện tích châu Á", "Toàn bộ diện tích châu Á"],
-    correctAnswer: "3/4 diện tích châu Á"
+    question: "Việt Nam tiếp giáp với hai đại dương nào sau đây?",
+    options: [
+      "Bắc Băng Dương và Ấn Độ Dương",
+      "Ấn Độ Dương và Thái Bình Dương",
+      "Thái Bình Dương và Đại Tây Dương",
+      "Đại Tây Dương và Bắc Băng Dương",
+    ],
+    correctAnswer: "Ấn Độ Dương và Thái Bình Dương",
   },
   {
     id: 15,
-    question: "Quốc gia đông dân nhất châu Á (năm 2020) là:",
-    options: ["Trung Quốc", "Thái Lan", "Việt Nam", "Ấn Độ"],
-    correctAnswer: "Trung Quốc"
+    question: "Ở Việt Nam, các cao nguyên ba dan phân bố ở vùng nào dưới đây?",
+    options: ["Đông Bắc", "Tây Bắc", "Bắc Trung Bộ", "Tây Nguyên"],
+    correctAnswer: "Tây Nguyên",
   },
   {
     id: 16,
-    question: "Quốc gia thường xuyên chịu ảnh hưởng nặng nề nhất của thiên tai động đất, núi lửa ở khu vực Đông Á là:",
-    options: ["Triều Tiên", "Trung Quốc", "Hàn Quốc", "Nhật Bản"],
-    correctAnswer: "Nhật Bản"
+    question: "Ở nước ta, dãy núi nào sau đây chạy theo hướng vòng cung?",
+    options: ["Con Voi", "Hoàng Liên Sơn", "Trường Sơn Bắc", "Ngân Sơn"],
+    correctAnswer: "Ngân Sơn",
   },
   {
     id: 17,
-    question: "Châu Âu có bao nhiêu khu vực địa hình chính?",
-    options: ["3 khu vực địa hình chính", "2 khu vực địa hình chính", "1 khu vực địa hình chính", "4 khu vực địa hình chính"],
-    correctAnswer: "2 khu vực địa hình chính"
+    question: "Số giờ nắng nước ta nhận được mỗi năm là:",
+    options: [
+      "1300 - 4000 giờ trong năm",
+      "1400 - 3500 giờ trong năm",
+      "1400 - 3000 giờ trong năm",
+      "1300 - 3500 giờ trong năm",
+    ],
+    correctAnswer: "1400 - 3000 giờ trong năm",
   },
   {
     id: 18,
-    question: "Hiện nay, Liên minh châu Âu (EU) có bao nhiêu thành viên?",
-    options: ["6", "26", "27", "28"],
-    correctAnswer: "27"
+    question: "Hằng năm, nước ta có lượng mưa trung bình khoảng:",
+    options: [
+      "1500 - 2000 mm/năm",
+      "1200 - 1800 mm/năm",
+      "1300 - 2000 mm/năm",
+      "1400 - 2200 mm/năm",
+    ],
+    correctAnswer: "1500 - 2000 mm/năm",
   },
-  {
-    id: 19,
-    question: "Lúc mới thành lập (1957) Liên minh châu Âu có tên gọi là:",
-    options: ["Khối thị trường chung châu Âu", "Cộng đồng châu Âu", "Cộng đồng kinh tế châu Âu", "Liên minh châu Âu"],
-    correctAnswer: "Cộng đồng kinh tế châu Âu"
-  },
-  {
-    id: 20,
-    question: "Số nước sáng lập ban đầu của Liên minh châu Âu là:",
-    options: ["5 nước", "6 nước", "7 nước", "8 nước"],
-    correctAnswer: "6 nước"
-  },
-  {
-    id: 21,
-    question: "Hiện nay, đô thị hóa ở châu Á đang có xu hướng di chuyển về:",
-    options: ["Ven biển", "Nội địa", "Miền núi", "Cao nguyên"],
-    correctAnswer: "Ven biển"
-  },
-  {
-    id: 22,
-    question: "Châu Á không tiếp giáp với đại dương nào?",
-    options: ["Đại Tây Dương", "Thái Bình Dương", "Ấn Độ Dương", "Bắc Băng Dương"],
-    correctAnswer: "Đại Tây Dương"
-  },
-  {
-    id: 23,
-    question: "Châu Á có nhiều dầu mỏ, khí đốt tập trung nhiều nhất ở khu vực nào?",
-    options: ["Nam Á", "Đông Á", "Tây Nam Á", "Bắc Á"],
-    correctAnswer: "Tây Nam Á"
-  },
-  {
-    id: 24,
-    question: "Phật giáo và Ấn Độ giáo ra đời ở khu vực nào của châu Á?",
-    options: ["Tây Nam Á", "Đông Á", "Bắc Á", "Nam Á"],
-    correctAnswer: "Nam Á"
-  },
-  {
-    id: 25,
-    question: "Dân cư châu Á tập trung chủ yếu ở khu vực đồng bằng ven biển vì:",
-    options: ["Có điều kiện tự nhiên thuận lợi", "Gần trung tâm kinh tế", "Gần nguồn khoáng sản", "Gần khu vực núi cao"],
-    correctAnswer: "Có điều kiện tự nhiên thuận lợi"
-  }
 ];
